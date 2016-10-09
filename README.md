2100bedc51a1f12ef0b5d7e955adf4a8c789ecd6
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2016-10-09 13:58:45 +0900

    Release the version 1.0.0 .

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
 # ansible-ghq-module
+
+[![Build Status](https://travis-ci.org/suzuki-shunsuke/ansible-ghq-module.svg?branch=master)](https://travis-ci.org/suzuki-shunsuke/ansible-ghq-module)
+
+Manage remote repository with [motemen/ghq](https://github.com/motemen/ghq).
+
+## Notice
+
+* This module doesn't support the check mode.
+* If this module succeeds, the result's changed attribute is always true.
+
+## Requirements
+
+* Go
+* motemen/ghq
+
+## Install
+
+In the following example this module is installed in ~/ansible/modules directory.
+
+```
+$ mkdir -p ~/ansible/modules
+$ export ANSIBLE_LIBRARY=~/ansible/modules
+$ git clone suzuki-shunsuke/ansible-ghq-module ~/ansible/modules/ansible-ghq-module
+```
+
+## Options
+
+parameter | required | default | choices | comments
+--- | --- | --- | --- | ---
+name | no | | | The repository path
+executable | no | | | The executable path of ghq command
+root | no | | | The environment variable GHQ_ROOT
+update | no | no | bool | If yes, the -u option is added
+ssh | no | no | bool | If yes, the -p option is added
+shallow | no | no | bool | If yes, the -shallow option is added
+src | no | | | The source path of "ghq import" command
+subcommand | no | get | get | The subcommand of "ghq import" command
+
+## Example
+
+```yaml
+# Clone suzuki-shunsuke/zsh.conf
+ghq:
+  name: suzuki-shunsuke/zsh.conf
+
+# Specify the path of ghq command
+ghq:
+  name: suzuki-shunsuke/zsh.conf
+  executable: {{ ansible_env.HOME }}/.go/bin/ghq
+
+# Update
+go:
+  name: suzuki-shunsuke/zsh.conf
+  update: yes
+
+# Via ssh
+ghq:
+  name: suzuki-shunsuke/zsh.conf
+  ssh: yes
+
+# Specify the ghq.root
+ghq:
+  name: suzuki-shunsuke/zsh.conf
+  root: {{ ansible_env.HOME }}/.repos
+
+# Shallow clone
+ghq:
+  name: suzuki-shunsuke/zsh.conf
+  shallow: yes
+
+# Import from file
+ghq:
+  src: repos.txt
+
+# Import via subcommand
+ghq:
+  subcommand: starred motemen
+
+# Subcommand is either str or list of str
+ghq:
+  subcommand: ["starred", "motemen"]
+```
+
+## Licence
+
+MIT
+
+## For developers
+
+### Local Test with Vagrant
+
+```
+$ sh _setup.sh
+$ export ANSIBLE_LIBRARY=$PWD:$ANSIBLE_LIBRARY  # if you use direnv, it is automatically set.
+$ vagrant up --provision
+```
---
+++ ghq.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+DOCUMENTATION = '''
+---
+module: ghq
+short_description: Manage remote repository with motemen/ghq.
+options:
+  name:
+    description:
+    - A repository url.
+    required: no
+    type: str
+  executable:
+    description:
+      - The executable location for ghq.
+    required: no
+    type: str
+  root:
+    description:
+      - The ghq root directory.
+    required: no
+    type: str
+  update:
+    description:
+      - When this option is yes, the -u option is added.
+    required: no
+    type: bool
+    default: no
+  ssh:
+    description:
+      - When this option is yes, the -p option is added.
+    required: no
+    type: bool
+    default: no
+  shallow:
+    description:
+      - When this option is yes, the -shallow option is added.
+    required: no
+    type: bool
+    default: no
+  src:
+    description:
+      - The source file path of ghq import command.
+    required: no
+    type: str
+  subcommand:
+    description:
+      - subcommand of ghq import. It includes arguments.
+    required: no
+    type: str or str[]
+requirements:
+- go
+- motemen/ghq
+author: "Suzuki Shunsuke"
+'''
+
+EXAMPLES = '''
+# Clone suzuki-shunsuke/zsh.conf
+ghq:
+  name: suzuki-shunsuke/zsh.conf
+
+# Specify the path of ghq command
+ghq:
+  name: suzuki-shunsuke/zsh.conf
+  executable: {{ ansible_env.HOME }}/.go/bin/ghq
+
+# Update
+go:
+  name: suzuki-shunsuke/zsh.conf
+  update: yes
+
+# Via ssh
+ghq:
+  name: suzuki-shunsuke/zsh.conf
+  ssh: yes
+
+# Specify the ghq.root
+ghq:
+  name: suzuki-shunsuke/zsh.conf
+  root: {{ ansible_env.HOME }}/.repos
+
+# Shallow clone
+ghq:
+  name: suzuki-shunsuke/zsh.conf
+  shallow: yes
+
+# Import from file
+ghq:
+  src: repos.txt
+
+# Import via subcommand
+ghq:
+  subcommand: starred motemen
+'''
+
+RETURN = '''
+'''
+
+import json
+import os.path
+
+# from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import *
+
+
+def main():
+    module = AnsibleModule(argument_spec={
+        "name": {"default": None, "type": "str"},
+        "executable": {"default": "ghq", "type": "str"},
+        "root": {"default": None, "type": "str"},
+        "update": {"default": False, "type": "bool"},
+        "ssh": {"default": False, "type": "bool"},
+        "shallow": {"default": False, "type": "bool"},
+        "src": {"default": None, "type": "str"},
+        "subcommand": {"default": None},
+    }, add_file_common_args=True)
+    params = module.params
+    name = params["name"]
+    executable = params["executable"]
+    root = params["root"]
+    update = params["update"]
+    ssh = params["ssh"]
+    shallow = params["shallow"]
+    src = params["src"]
+    subcommand = params["subcommand"]
+
+    # name and src and subcommand are specified only one and .
+    if sum(1 for a in [name, src, subcommand] if a) != 1:
+        module.fail_json(msg="name and src and subcommand must be specified only one.")
+
+    # subcommand is either str or list
+    if subcommand:
+        if not isinstance(subcommand, [str, list]):
+            module.fail_json(msg="subcommand must be str or list of str.")
+        if isinstance(subcommand, list) and not all(isinstance(s, str) for s in subcommand):
+            module.fail_json(msg="subcommand must be str or list of str.")
+
+    options = {}
+    environ_update = {}
+    if root:
+        environ_update["GHQ_ROOT"] = root
+    if environ_update:
+        options["environ_update"] = environ_update
+
+    cmd = [executable, "get" if name else "import"]
+    if update:
+        cmd.append("-u")
+    if ssh:
+        cmd.append("-p")
+    if shallow:
+        cmd.append("-shallow")
+
+    if name:
+        cmd.append(name)
+    elif src:
+        src = os.path.expanduser(src)
+        if not os.path.exists(src):
+            module.fail_json(msg="Source {} not found".format(src))
+        if not os.access(src, os.R_OK):
+            module.fail_json(msg="Source {} not readable".format(src))
+        if os.path.isdir(src):
+            module.fail_json(msg="Source must be file, not directory")
+        with open(src) as r:
+            options["data"] = r.read()
+    elif subcommand:
+        cmd += subcommand if isinstance(subcommand, list) else subcommand.split(" ")
+
+    rc, out, err = module.run_command(cmd, **options)
+    if rc:
+        module.fail_json(msg=err, stdout=out, cmd=cmd, options=options)
+    else:
+        module.exit_json(changed=True, stdout=out, stderr=err)
+
+
+if __name__ == '__main__':
+    main()
